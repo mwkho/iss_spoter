f5a586a5a781ab7400dfd69aa10035b92dcd8252
mwkho <martin.h@hotmail.ca>
2021-03-13 06:08:26 +0000

    completed all 3 promised version of the functions

---
+++ iss_promised.js
@@ -0,0 +1,19 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request('https://api.ipify.org?format=json')
+};
+
+
+const fetchCoordsByIP = (body) => {
+  const IP = JSON.parse(body);
+  return request(`https://freegeoip.app/json/${IP.ip}`);
+};
+
+const fetchISSFlyOverTimes = (data) => {
+  const coords = {};
+  coords["latitude"] = JSON.parse(data)["latitude"];
+  coords["longitude"] = JSON.parse(data)["longitude"];
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${data.latitude}&lon=${data.longitude}`);
+}
+module.exports = {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes}
